f63425822428a0400164ad57d6b2af82922037d6
Jane <mimimeta@bk.ru>
2025-02-14 20:02:31 +0400

    review v4 pep8

--- yacut/models.py
+++ yacut/models.py
@@ -16,7 +16,9 @@ class URLMap(db.Model):
     """Модель для хранения информации о сокращенных URL."""
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(MAX_URL_LENGTH), nullable=False)
-    short = db.Column(db.String(MAX_LENGTH_USERS_SHORT), nullable=False, unique=True)
+    short = db.Column(db.String(
+        MAX_LENGTH_USERS_SHORT), nullable=False, unique=True
+    )
     timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def to_dict(self):
